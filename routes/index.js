4433e635aeb6a8bbfd5ef1f19ac9f831a26f7e53
JoshChun <joshuachun@outlook.com>
2021-03-22 19:40:56 +1100

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'keyBite' });
+});
+
+router.get('/cases', function(req, res, next) {
+  res.render('cases', { title: 'cases' });
 });
 
 module.exports = router;
